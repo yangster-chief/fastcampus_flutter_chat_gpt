ec9546c280a002a96bd45ac6c7ec317920ac6099
thunder-ramgee <38216680+yangster-chief@users.noreply.github.com>
2024-02-17 04:16:02 +0900

    add chat list page

--- lib/page/chat_list_page/chat_list_page.dart
+++ lib/page/chat_list_page/chat_list_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_chat_gpt/hive_model/chat_item_model.dart';
+import 'package:flutter_chat_gpt/page/chat_list_page/bloc/chat_list_bloc.dart';
+import 'package:flutter_chat_gpt/page/chat_list_page/widgets/chat_item.dart';
+import 'package:flutter_chat_gpt/page/chat_page/chat_page.dart';
 
 ///
 /// flutter_chat_gpt
@@ -13,6 +18,34 @@ class ChatListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Chat List')),
+      body: BlocBuilder<ChatListBloc, ChatListState>(
+        builder: (context, state) => state.chats.isEmpty
+            ? const Center(child: Text('No chats yet'))
+            : ListView.builder(
+                itemCount: state.chats.length,
+                itemBuilder: (context, index) => ChatItem(
+                    item: state.chats[index],
+                    onTapped: () => _openChat(state.chats[index], context),
+                    onDelete: () => _deleteChat(index, context)),
+              ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          BlocProvider.of<ChatListBloc>(context).add(const ChatListAddEvent());
+        },
+        label: const Text('Add New Chat'),
+        icon: const Icon(Icons.message_outlined),
+      ),
+    );
+  }
+
+  void _openChat(ChatItemModel chatItem, BuildContext context) {
+    Navigator.push(context, MaterialPageRoute(builder: (_) => ChatPage()));
+  }
+
+  void _deleteChat(int index, BuildContext context) {
+    BlocProvider.of<ChatListBloc>(context).add(ChatListRemoveEvent(index));
   }
 }
--- lib/page/chat_list_page/widgets/chat_item.dart
+++ lib/page/chat_list_page/widgets/chat_item.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_gpt/hive_model/chat_item_model.dart';
+
 ///
 /// flutter_chat_gpt
 /// File Name: chat_item
@@ -5,3 +8,23 @@
 ///
 /// Description:
 ///
+class ChatItem extends StatelessWidget {
+  final ChatItemModel item;
+  final VoidCallback onTapped;
+  final VoidCallback onDelete;
+
+  const ChatItem({
+    super.key,
+    required this.item,
+    required this.onTapped,
+    required this.onDelete,
+  });
+
+  @override
+  Widget build(BuildContext context) => ListTile(
+        title: Text(item.title),
+        onTap: onTapped,
+        trailing:
+            IconButton(icon: const Icon(Icons.delete), onPressed: onDelete),
+      );
+}
